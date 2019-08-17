20767eb83d66466816dc18f8126e3c8478f5b526
Luis Borjas <luisfborjas@gmail.com>
2019-08-17 15:58:15 -0400

    haskell/stack

--- emacs.d/customizations/setup-haskell.el
+++ emacs.d/customizations/setup-haskell.el
@@ -1,3 +1,5 @@
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(custom-set-variables '(haskell-process-type 'stack-ghci)) ; needed for Stack-based projects only
+
