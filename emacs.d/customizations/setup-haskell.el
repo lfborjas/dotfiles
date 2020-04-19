d5e68457cb16a38f5955fd10ec51ea0ba9add987
Luis Borjas <luisfborjas@gmail.com>
2020-04-19 12:35:49 -0400

    Make haskell-compile use stack
    As per:
    
    http://haskell.github.io/haskell-mode/manual/latest/Compilation.html#Compilation

--- emacs.d/customizations/setup-haskell.el
+++ emacs.d/customizations/setup-haskell.el
@@ -3,4 +3,5 @@
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (custom-set-variables '(haskell-process-type 'stack-ghci)) ; needed for Stack-based projects only
 (custom-set-variables
- '(haskell-stylish-on-save t))
+ '(haskell-stylish-on-save t)
+ '(haskell-compile-cabal-build-command "stack build"))
